c34a0cf49883621a97209c796bd49cdd700fdfc8
natchanon37 <natchanon281137@gmail.com>
2022-07-18 17:38:01 +0900

    Modify: page can't scroll when berger bottom open

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 const btnHamberger = document.querySelector("#btnHamberger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeEle = document.querySelectorAll(".has-fade");
@@ -9,12 +10,14 @@ header.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     //close the hamberger menu
     header.classList.remove("open");
+    body.classList.remove("no-scroll");
     fadeEle.forEach((element) => {
       element.classList.remove("fade-in");
       element.classList.add("fade-out");
     });
   } else {
     header.classList.add("open"); // open the hamberger menu
+    body.classList.add("no-scroll");
     fadeEle.forEach((element) => {
       element.classList.add("fade-in");
       element.classList.remove("fade-out");
